155d7c6e4d8fba20f2eb7fa287db4b9398aa1f3e
Your Name <you@example.com>
2021-03-28 17:12:21 -0500

    commit

---
+++ Semana2/sesion2/IniciosMYSQL.sql
@@ -0,0 +1,19 @@
+--tipos de datos
+
+int
+float
+
+--Tiempo Fecha
+date : su formato es YYYY-MM-DD
+tiemstamp : devulve fecha y hora
+time : HH_MM:SS
+
+-- string
+char(8) :
+-- atributo que adminte caracteres
+varchar(80) 
+-- tipo de dato tipo texto
+
+boolean 
+-- true
+--  false
