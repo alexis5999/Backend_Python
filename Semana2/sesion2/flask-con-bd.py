6951926029c84d9d0e5471c5cc77f4d95a9551ff
Your Name <you@example.com>
2021-03-28 17:11:48 -0500

    commit

--- Semana2/sesion2/flask-con-bd.py
+++ Semana2/sesion2/flask-con-bd.py
@@ -4,10 +4,12 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_HOST'] ='localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345'
 app.config['MYSQL_DB'] = 'test'
 
 msql = MySQL(app)
 @app.route('/')
 def inicio():
     return 'La api funciona correctamente'
+
+app.run(debug=True)
